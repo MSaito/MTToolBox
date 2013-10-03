7ef916d7846de74fd234da54eee014a892d1e537
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2013-10-03 17:33:20 +0900

    AlgorithmBestBits arrange

--- include/MTToolBox/AlgorithmBestBits.hpp
+++ include/MTToolBox/AlgorithmBestBits.hpp
@@ -46,8 +46,8 @@ namespace MTToolBox {
     public:
         /** テンパリングパラメータ */
         T * param;
-        /** 均等分布次元 */
-        int kv;
+        /** 均等分布次元の理論値との差の総和 */
+        int delta;
         /** テンパリングパラメータの数 */
         int size;
 
@@ -57,6 +57,7 @@ namespace MTToolBox {
          */
         temper_params(int param_num) {
             size = param_num;
+            delta = 0;
             param = new T[param_num];
             for (int i = 0; i < param_num; i++) {
                 param[i] = 0;
@@ -69,28 +70,11 @@ namespace MTToolBox {
         ~temper_params() {
             delete[] param;
         }
-        /**
-         * kv を無視した比較
-         * @param that 比較対象
-         */
-        bool eql(temper_params<T>& that) {
-            if (size != that.size) {
-                return false;
-            }
-            bool result = true;
-            for (int i = 0; i < size; i++) {
-                if (param[i] != that.param[i]) {
-                    result = false;
-                    break;
-                }
-            }
-            return result;
-        }
 #if defined(DEBUG)
         string toString() {
             stringstream ss;
             ss << "[";
-            ss << dec << kv << ":";
+            ss << dec << delta << ":";
             for (int i = 0; i < size; i++) {
                 ss << hex << setw(8) << setfill('0') << param[i] << ",";
             }
@@ -131,6 +115,8 @@ namespace MTToolBox {
          * 正は左シフト。現状では右シフトには対応していない。
          * @param[in] param_num テンパリングパラメータの数, MTDCでは2。
          * 7を越えないこと。実際には 2 の場合しかテストしていない。
+         * @param[in] limit_v テンパリングパラメータを上位何ビットまでテンパリングするか。
+         * ただし、この値+以後のシフト量だけテンパリングする。
          */
         AlgorithmBestBits(int out_bit_length,
                           const int shift_values[],
@@ -161,7 +147,6 @@ namespace MTToolBox {
          * ンパリングパラメータを決定する。求められたよいテンパリングパ
          * ラメータは疑似乱数生成器にセットされる。
          *
-         * @tparam T 疑似乱数生成器の出力の型
          * @param[in,out] rand 疑似乱数生成器
          * @param[in] verbose 余分な情報を出力するフラグ
          * @returns 0
@@ -173,18 +158,17 @@ namespace MTToolBox {
                 cout << "searching from MSB" << endl;
             }
             shared_ptr<tempp> initial(new tempp(size));
-            initial->kv = 0;
+            initial->delta = 0;
             for (int i = 0; i < size; i++) {
                 initial->param[i] = 0;
             }
 
             vector<shared_ptr<tempp> > params;
             params.push_back(initial);
-            int kv;
+            int delta;
 #if defined(DEBUG)
             cout << "DEBUG: bit_len = " << dec << bit_len << endl;
 #endif
-//            int min_shift = get_min_shift(shifts, size);
             for (int p = 0; p < limit; p++) {
                 vector<shared_ptr<tempp> > current;
                 current.clear();
@@ -192,75 +176,25 @@ namespace MTToolBox {
                     search_best_temper(rand, p, *params[i],
                                        current, verbose);
                 }
-#if defined(DEBUG)
-                cout << "DEBUG: current:size = " << dec << current.size()
-                     << endl;
+                delta = rand.bitSize() * obSize;
                 for (unsigned int i = 0; i < current.size(); i++) {
-                    cout << "DEBUG:current[" << dec << i << "]:"
-                         << current[i]->toString() << endl;
-                }
-#endif
-                kv = -1;
-                for (unsigned int i = 0; i < current.size(); i++) {
-                    if (current[i]->kv > kv) {
-                        kv = current[i]->kv;
+                    if (current[i]->delta < delta) {
+                        delta = current[i]->delta;
                     }
-#if defined(DEBUG) && 0
-                    else {
-                        cout << "DEBUG: current[i]->kv = " << dec
-                             << current[i]->kv << endl;
-                    }
-#endif
                 }
 #if defined(DEBUG)
-                cout << "DEBUG: kv = " << dec << kv << endl;
+                cout << "DEBUG: delta = " << dec << delta << endl;
 #endif
                 if (verbose) {
-                    cout << "kv = " << dec << kv << endl;
+                    cout << "delta = " << dec << delta << endl;
                 }
                 params.clear();
-                //T mask = 0;
-                //mask = (~mask) << (obSize - p - 1);
                 for (unsigned int i = 0; i < current.size(); i++) {
-                    if (current[i]->kv == kv) {
-                        //params.push_back(current[i]);
-#if 0
-                        for (int j = 0; j < size; j++) {
-                            current[i]->param[j] &= mask;
-                        }
-#endif
-                        push_if_notexist(params, current[i]);
+                    if (current[i]->delta == delta) {
+                        params.push_back(current[i]);
                     }
                 }
-#if defined(DEBUG)
-                cout << "DEBUG: params:size = " << dec << params.size()
-                     << endl;
-                for (unsigned int i = 0; i < params.size(); i++) {
-                    cout << "DEBUG: params[" << dec << i << "]:"
-                         << params[i]->toString() << endl;
-                }
-#endif
             }
-#if 0
-            // kv を最大にするテンパリングパラメータの中でハミングウェイト最大
-            // のものをテンパリングパラメータにする。(複数のなかで最初に見つかったもの)
-            int hamming = -1;
-            int index = -1;
-            for (unsigned int i = 0; i < params.size(); i++) {
-                int h = 0;
-#if defined(DEBUG)
-                cout << "DEBUG:params[" << dec << i << "]"
-                     << params[i]->toString() << endl;
-#endif
-                for (int j = 0; j < size; j++) {
-                    h += count_bit(params[i]->param[j]);
-                }
-                if (hamming < h) {
-                    hamming = h;
-                    index = i;
-                }
-            }
-#endif
             T mask = 0;
             mask = ~mask;
             for (int i = 0; i < size; i++) {
@@ -269,7 +203,7 @@ namespace MTToolBox {
             }
             rand.setUpTempering();
             if (verbose) {
-                cout << "kv = " << dec << kv << endl;
+                cout << "delta = " << dec << delta << endl;
             }
             rand.resetReverseOutput();
             return 0;
@@ -283,9 +217,9 @@ namespace MTToolBox {
         int bit_len;
         int size;
         int obSize;
-//        bool lsb;
         int * shifts;
         int num_pat;
+
         /**
          * テンパリングパラメータを探索する。
          *
@@ -293,7 +227,6 @@ namespace MTToolBox {
          * @param[in] v_bit 今からテンパリングしようとするビット(上から数えた)
          * @param[in] para v_bit -1 ビット目までのテンパリングパラメータ
          * @param[out] current v ビット目のテンパリングパラメータとデルタのvector
-         * @param[in] shifts シフト量の配列
          * @param[in] verbose true なら余計な情報を標準出力に表示する。
          */
         void search_best_temper(TemperingCalculatable<T>& rand,
@@ -301,7 +234,7 @@ namespace MTToolBox {
                                 const tempp& para,
                                 vector<shared_ptr<tempp> >& current,
                                 bool verbose) {
-            int kv = -1;
+            int delta = rand.bitSize() * obSize;
             T mask = 0;
             mask = ~mask;
             // size が 2 なら 11, 10, 01, 00 の4パターン
@@ -314,18 +247,18 @@ namespace MTToolBox {
                 shared_ptr<tempp> pattern(new tempp(size));
                 make_pattern(*pattern, i, v_bit, para);
 #if defined(DEBUG)
-                cout << "*pattern:" << pattern->toString() << endl;
+                cout << "pattern:" << pattern->toString() << endl;
 #endif
                 for (int j = 0; j < size; j++) {
                     rand.setTemperingPattern(mask, pattern->param[j], j);
                 }
-                pattern->kv = get_equidist(rand, v_bit + 1);
+                pattern->delta = get_equidist(rand, v_bit + 1);
                 if (verbose) {
-                    cout << "pattern->kv:" << dec << pattern->kv << endl;
+                    cout << "pattern->delta:" << dec << pattern->delta << endl;
                 }
-                if (pattern->kv >= kv) {
-                    push_if_notexist(current, pattern);
-                    kv = pattern->kv;
+                if (pattern->delta <= delta) {
+                    current.push_back(pattern);
+                    delta = pattern->delta;
                 }
             }
         }
@@ -333,19 +266,15 @@ namespace MTToolBox {
         /**
          * AlgorithmEquidistribution#get_equidist()のラッパー
          *
-         * @param rand linear generator
-         * @param bit_len_ \b bit_len_ MSBs of equidistribution
-         * property of the generator is calculated.
-         * @param bitSize Mersenne Exponent of the period of rand.
-         * @returns summation of equidistribution property
-         * from 0 to \b bit_len_ -1 MSBs.
+         * @param rand 疑似乱数生成器
+         * @param bit_len_ \b bit_len_ 長の MSBs の均等分布次元を計算する
+         * @returns 均等分布次元の理論値との差を0からbit_lenまで合計したもの
          */
         int get_equidist(TemperingCalculatable<T>& rand,
                          int bit_length) {
             AlgorithmEquidistribution<T> sb(rand, bit_length);
             int veq[bit_length];
-            sb.get_all_equidist(veq);
-            return veq[bit_length - 1];
+            return sb.get_all_equidist(veq);
         }
 
         /**
@@ -361,7 +290,7 @@ namespace MTToolBox {
          */
         void make_pattern(tempp& result,
                           int pat, int v, const tempp& para) const {
-            result.kv = 0;
+            result.delta = 0;
             for (int i = 0; i < result.size; i++) {
                 result.param[i] = para.param[i];
             }
@@ -378,23 +307,7 @@ namespace MTToolBox {
                     result.param[index] |= (one << (obSize - v - sum - 1))
                     & para_mask;
                 } else if (((pat & mask) == 0) && (obSize > v + sum + 1)) {
-#if defined(DEBUG)
-                    cout << "make_pattern:" << dec << pat << ","
-                         << dec << v << "," << result.toString()
-                         << hex << para_mask
-                         << "," << dec << sum << endl;
-#endif
                     result.param[index] &= ~(one << (obSize - v - sum - 1));
-#if defined(DEBUG)
-                    cout << "make_pattern:" << dec << pat << ","
-                         << dec << v << "," << result.toString()
-                         << hex << para_mask
-                         << "," << dec << sum
-                         << "," << hex << ~(one << (obSize - v - sum - 1))
-                         << "," << dec << v + sum + 1
-                         << "," << obSize
-                         << endl;
-#endif
                 }
                 mask = mask >> 1;
                 index = index + 1;
@@ -421,17 +334,9 @@ namespace MTToolBox {
          *
          * @param[in] pat ビットパターン
          * @param[in] v ビット位置
-         * @param[in] shifts シフト量
          * @return true 該当する場合
          */
         bool inRange(int pat, int v) const {
-#if 0
-            for (int i = 0; i < size; i++) {
-                if ((obSize < v + shifts[i]) && ((pat >> i) != 0)) {
-                    return false;
-                }
-            }
-#endif
             int index = 0;
             int idx = 0;
             int rdx = size - 1;
@@ -451,30 +356,6 @@ namespace MTToolBox {
                 }
             }
             return true;
-        }
-
-        int get_min_shift(int nums[], int length) {
-            int min = 5000;
-            for (int i = 0; i < length; i++) {
-                if (min > nums[i]) {
-                    min = nums[i];
-                }
-            }
-            return min;
-        }
-
-        void push_if_notexist(vector<shared_ptr<tempp> >& vec_temp,
-                              shared_ptr<tempp>& pattern) {
-            bool found = false;
-            for (unsigned int i = 0; i < vec_temp.size(); i++) {
-                if (pattern->eql(*vec_temp[i])) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                vec_temp.push_back(pattern);
-            }
         }
     };
 }
