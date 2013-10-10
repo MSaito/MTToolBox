8485a8965fae61d81576a0857f35ec8652a2fc4f
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2013-10-10 15:20:15 +0900

    add English Document for AlgorithmRecursionAndTempering

--- include/MTToolBox/AlgorithmRecursionAndTempering.hpp
+++ include/MTToolBox/AlgorithmRecursionAndTempering.hpp
@@ -3,7 +3,21 @@
 /**
  * @file AlgorithmRecursionAndTempering.hpp
  *
+ *\japanese
  * @brief 状態遷移パラメータとテンパリングパラメータの探索を一度に行う
+ *\endjapanese
+ *
+ *\english
+ * <ol>
+ * <li> Search parameters of state transion function of pseudo
+ * random number generator so that the characteristic polynomial
+ * of the funciton will have max degree and will be
+ * primitive.</li>
+ *<li> Search tempering parameters to improve dimension of
+ * equi-distribution of output of pseudo random number
+ * generator.</li>
+ *</ol>
+ *\endenglish
  *
  * @author Mutsuo Saito (Hiroshima University)
  * @author Makoto Matsumoto (Hiroshima University)
@@ -24,7 +38,6 @@
 #include <cerrno>
 #include <unistd.h>
 #include <time.h>
-#include <tr1/memory>
 #include <MTToolBox/TemperingCalculatable.hpp>
 #include <MTToolBox/AlgorithmEquidistribution.hpp>
 #include <MTToolBox/AlgorithmRecursionSearch.hpp>
@@ -34,18 +47,52 @@
 namespace MTToolBox {
     /**
      * @class AlgorithmRecursionAndTempering
-     * - 状態遷移関数の特性多項式が原始多項式となるような疑似乱数生成器のパラメータを探索する。
-     * - 均等分布次元がよくなるようなテンパリングパラメータを探索する。
+     *\japanese
+     * # 状態遷移関数の特性多項式が原始多項式となるような疑似乱数生成器のパラメータを探索する。
+     * # 均等分布次元がよくなるようなテンパリングパラメータを探索する。
      *
-     * @tparam T 疑似乱数生成器の出力の型
+     * @tparam U 疑似乱数生成器の出力の型, 符号なし型であること
+     *\endjapanese
+     *
+     *\english
+     * <ol>
+     * <li> Search parameters of state transion function of pseudo
+     * random number generator so that the characteristic polynomial
+     * of the funciton will have max degree and will be
+     * primitive.</li>
+     *<li> Search tempering parameters to improve dimension of
+     * equi-distribution of output of pseudo random number
+     * generator.</li>
+     *</ol>
+     *
+     * @tparam U Type of output of pseudo random number
+     * generator. Should be unsigned number.
+     * \endenglish
      */
-    template<typename T>
+    template<typename U>
     class AlgorithmRecursionAndTempering {
     public:
-        AlgorithmRecursionAndTempering(AbstractGenerator<T>& bg) {
+        /**
+         *\japanese
+         * コンストラクタ
+         * @param bg パラメータをランダムサーチするための疑似乱数生成器を指定する。
+         * この疑似乱数生成器に限り、GF(2)線形である必要はない。疑似乱数生成器である
+         * 必要すらない。
+         *\endjapanese
+         *
+         *\english
+         * Constructor
+         * @param bg a generator used for generating numbers to make
+         * parameters. This generator is not need to be GF(2)-linear
+         * pseudo random number generator, for example, TinyMTDC in
+         * sample directory uses sequential counter.
+         *\endenglish
+         */
+        AlgorithmRecursionAndTempering(AbstractGenerator<U>& bg) {
             baseGenerator = &bg;
         }
         /**
+         *\japanese
          * 状態遷移パラメータとテンパリングパラメータを探索する。
          *
          * @param lg テンパリングパラメータ計算可能な疑似乱数生成器
@@ -54,11 +101,27 @@ namespace MTToolBox {
          * @param verbose 余分な情報を出力するフラグ
          * @param os 出力ストリーム
          * @param no_lsb LSBからのテンパリングをしない
-         * @return 原始多項式を発見した
+         * @return true 原始多項式を発見しテンパリングパラメータを設定した場合
+         *\endjapanese
+         *
+         *\english
+         * Search parameters for state transition function and
+         * parameters for tempering.
+         *
+         *\endenglish
+         * @param lg GF(2)-linear pseudo random number generator
+         * whose parameters are to be searched.
+         * @param st1 Algorithm for searching tempering parameters.
+         * @param st2 Algorithm for searching tempering parameters from LSB.
+         * @param verbose if true redundant messages will be outputed.
+         * @param os output stream for redundant messages.
+         * @param no_lsb if true, \b st2 will not be used.
+         * @return false if no tempering parameters which gives proper
+         * state transition function are found.
          */
-        bool search(TemperingCalculatable<T>& lg,
-                    AlgorithmTempering<T>& st1,
-                    AlgorithmTempering<T>& st2,
+        bool search(TemperingCalculatable<U>& lg,
+                    AlgorithmTempering<U>& st1,
+                    AlgorithmTempering<U>& st2,
                     bool verbose = false,
                     std::ostream& os = std::cout,
                     bool no_lsb = false) {
@@ -67,8 +130,8 @@ namespace MTToolBox {
             using namespace std::tr1;
 
             out = &os;
-            int veq[bit_size<T>()];
-            AlgorithmRecursionSearch<T> search(lg, *baseGenerator);
+            int veq[bit_size<U>()];
+            AlgorithmRecursionSearch<U> search(lg, *baseGenerator);
             int mexp = lg.bitSize();
             bool found = false;
             for (int i = 0;; i++) {
@@ -94,10 +157,9 @@ namespace MTToolBox {
             poly = search.getMinPoly();
             weight = NTL::weight(poly);
             if (verbose) {
-                AlgorithmEquidistribution<T> sb(lg, bit_size<T>());
-                //print_binary(*out, poly);
+                AlgorithmEquidistribution<U> sb(lg, bit_size<U>());
                 int delta = sb.get_all_equidist(veq);
-                print_kv(veq, mexp, bit_size<T>());
+                print_kv(veq, mexp, bit_size<U>());
                 *out << "delta = " << dec << delta << endl;
             }
             if (! no_lsb) {
@@ -106,57 +168,109 @@ namespace MTToolBox {
                     if (st2.isLSBTempering()) {
                         lg.setReverseOutput();
                     }
-                    AlgorithmEquidistribution<T> sc(lg, bit_size<T>());
+                    AlgorithmEquidistribution<U> sc(lg, bit_size<U>());
                     delta = sc.get_all_equidist(veq);
                     lg.resetReverseOutput();
                     time_t t = time(NULL);
                     *out << "lsb tempering parameters are found at "
                          << ctime(&t) << endl;
-                    print_kv(veq, mexp, bit_size<T>());
+                    print_kv(veq, mexp, bit_size<U>());
                     *out << "lsb delta = " << dec << delta << endl;
                 }
             }
             st1(lg, verbose);
-            AlgorithmEquidistribution<T> sc(lg, bit_size<T>());
+            AlgorithmEquidistribution<U> sc(lg, bit_size<U>());
             delta = sc.get_all_equidist(veq);
             if (verbose) {
                 time_t t = time(NULL);
                 *out << "tempering parameters are found at " << ctime(&t)
                      << endl;
                 *out << lg.getParamString() << endl;
-                print_kv(veq, mexp, bit_size<T>());
+                print_kv(veq, mexp, bit_size<U>());
                 *out << "delta = " << dec << delta << endl;
             }
             return true;
         }
 
         /**
+         *\japanese
+         *
          * MSBからの均等分布次元のみを向上させたい場合の探索を行う。
+         * 状態遷移関数のパラメータは探索する。
          *
          * @param lg テンパリングパラメータ計算可能な疑似乱数生成器
          * @param st テンパリングパラメータ探索アルゴリズム
          * @param verbose 余分な情報を出力するフラグ
          * @param os 出力ストリーム
          * @return 特性多項式が原始多項式となるような状態遷移パラメータを発見した
+         *\endjapanese
+         *
+         *\english
+         *
+         * Simple wrapper for users who want to search tempering
+         * parameter only from MSB.
+         *
+         * Parameters for state transition function are searched.
+         *
+         * @param lg GF(2)-linear pseudo random number generator
+         * whose parameters are to be searched.
+         * @param st Algorithm for searching tempering parameters.
+         * @param verbose if true redundant messages will be outputed.
+         * @param os output stream for redundant messages.
+         * @return false if no tempering parameters which gives proper
+         * state transition function are found.
+         *\endenglish
          */
-        bool search(TemperingCalculatable<T>& lg,
-                    AlgorithmTempering<T>& st,
+        bool search(TemperingCalculatable<U>& lg,
+                    AlgorithmTempering<U>& st,
                     bool verbose = false,
                     std::ostream& os = std::cout) {
             return search(lg, st, st, verbose, os, true);
         }
 
-        /** 特性多項式のハミングウェイトを返す */
+        /**
+         *\japanese
+         * 特性多項式のハミングウェイトを返す
+         * @return 特性多項式のハミングウェイト
+         *\endjapanese
+         *
+         *\english
+         * Returns Hamming weight of characteristic polynomial of state
+         * transition function.
+         * @return Hamming weight of characteristic polynomial.
+         *\endenglish
+         */
         int getWeight() {
             return weight;
         }
 
-        /** 均等分布次元の理論値との差の総和を返す。*/
+        /**
+         *\japanese
+         * 均等分布次元の理論値との差の総和を返す。
+         * @return 均等分布次元の理論値との差の総和
+         *\endjapanese
+         *
+         *\english
+         * returns sum of d(v)s, which are difference between k(v)
+         * and theoretical upper bound.
+         * @return sum of d(v)s.
+         *\endenglish
+         */
         int getDelta() {
             return delta;
         }
 
-        /** 状態遷移関数の特性多項式を返す。*/
+        /**
+         *\japanese
+         * 状態遷移関数の特性多項式を返す。
+         * @return 状態遷移関数の特性多項式
+         *\endjapanese
+         *
+         *\english
+         * Returns characteristic polynomial of state transition function.
+         * @return characteristic polynomial of state transition function.
+         *\endenglish
+         */
         const NTL::GF2X& getCharacteristicPolynomial() {
             return poly;
         }
@@ -165,7 +279,7 @@ namespace MTToolBox {
         int delta;
         NTL::GF2X poly;
         std::ostream * out;
-        AbstractGenerator<T> * baseGenerator;
+        AbstractGenerator<U> * baseGenerator;
 
         void print_kv(int veq[], int mexp, int size) {
             using namespace std;
