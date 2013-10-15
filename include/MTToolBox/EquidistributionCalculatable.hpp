7a81b86890a7e2d4d5b79a98ae5b58dc666a963a
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2013-10-15 15:55:59 +0900

    add English Document for EquidistributionCalculatable

--- include/MTToolBox/EquidistributionCalculatable.hpp
+++ include/MTToolBox/EquidistributionCalculatable.hpp
@@ -2,7 +2,14 @@
 #define MTTOOLBOX_EQUIDISTRIBUTION_CALCULATABLE_HPP
 /**
  * @file EquidistributionCalculatable.hpp
+ *\japanese
  * @brief このクラスはGF(2)線形疑似乱数生成器の均等分布次元を計算するためのクラスである。
+ *\endjapanese
+ *
+ *\english
+ * @brief This class is an Abstract class for calculating dimension of
+ * equi-distribution for GF(2)-linear pseudo random number generators.
+ *\endenglish
  *
  *
  */
@@ -13,6 +20,7 @@
 namespace MTToolBox {
     /**
      * @class EquidistributionCalculatable
+     *\japanese
      * @brief このクラスはGF(2)線形疑似乱数生成器の均等分布次元を計算するためのクラスである。
      *
      * GF(2)線形疑似乱数生成器を設計するならば、均等分布次元を計算して
@@ -25,60 +33,112 @@ namespace MTToolBox {
      * きな疑似乱数生成器を開発し、均等分布次元の値を大きくすることの方
      * が理論的上限に近いかどうかよりも重要である。
      *
-     * @tparam U 疑似乱数生成器の出力のタイプ
+     * @tparam U 疑似乱数生成器の出力のタイプ、符号なし型でなければならない。
+     *\endjapanese
+     *
+     *\english
+     * @brief This class is an Abstract class for calculating dimension of
+     * equi-distribution for GF(2)-linear pseudo random number generators.
+     *
+     * @tparam U type of output of pseudo random number generator, should
+     * be unsinged number.
+     *\endenglish
      */
     template<class U>
     class EquidistributionCalculatable : public RecursionSearchable<U> {
     public:
         using AbstractGenerator<U>::generate;
         /*
+         *\japanese
          * 自分のコピーを返す。
          *
          * @note clone には問題があるが、テンプレートよりよいと判断する。
          * @return 自分自身のコピー
+         *\endjapanese
+         *
+         *\english
+         * Return copy of myself.
+         * @return copy of myself.
+         *\endenglish
          */
         virtual EquidistributionCalculatable<U> * clone() const = 0;
 
         /*
-         * 上位(MSBから) @code outBitLen だけ出力する。
+         *\japanese
+         * 上位(MSBから) \b outBitLen だけ出力する。
          *
          * それ以外のビットは0でなければならない。
          * 均等分布次元の計算にとって重要なメソッドである。
          * このメソッドを実行すると、状態空間の遷移も行われる。
          * @param[in] outBitLen 上位から何ビット出力するかを指定する。
-         * @return 上位 @code outBitLen 以外は0の出力
+         * @return 上位 \b outBitLen 以外は0の出力
+         *\endjapanese
+         *
+         *\english
+         * output \b outBitLen from MSB.
+         *
+         * Remained bits should be all zeros.
+         * Users should call statte transition function inside
+         * this method.
+         * @param[in] outBitLen Shows how many bits are outputed.
+         * @return an integer whose \b outBiteLen bits from MSB.
+         *\endenglish
          */
         virtual U generate(int outBitLen) = 0;
 
         /**
+         *\japanese
          * GF(2)線形疑似乱数生成器の状態空間を加算し、内部状態を変更する。
          *
-         * \b 注意： 配列を使ってラウンドロビン方式で状態空間を保持している場合、
+         * \warning 配列を使ってラウンドロビン方式で状態空間を保持している場合、
          * このメソッドを実装する際に、インデックスを一致させて加算することが重要である。
          * \b state を状態配列、\b size を配列のサイズ、\b index をラウンドロビンの
          * インデックスとすると以下のような加算が必要になる。
+         *\endjapanese
+         *
+         *\english
+         * Add internal state of GF(2)-linear pseudo random number generators.
+         *
+         * \warning When keeping internal state in an array and using index
+         * for round robbin, adding shoud be relative to index like:
+         *\endenglish
          * @verbatim
-           for (int i = 0; i < size; i++) {
-             state[(index + i) % size] ^= that.state[(that.index + i) % size];
-           }
-          @endverbatim
+         for (int i = 0; i < size; i++) {
+           state[(index + i) % size] ^= that.state[(that.index + i) % size];
+         }
+         @endverbatim
          */
         virtual void add(EquidistributionCalculatable& that) = 0;
 
         /**
+         *\japanese
          * 状態空間をすべてゼロにセットする。
+         *\endjapanese
+         *
+         *\english
+         * Set all zero to internal state.
+         *\endenglish
          */
         virtual void setZero() = 0;
 
         /**
+         *\japanese
          * 状態空間がすべてゼロかチェックする。
          *
-         * \b 注意： メルセンヌツイスタのように、欠けた配列を使用する場合は、
+         * \warning メルセンヌツイスタのように、欠けた配列を使用する場合は、
          * 真の状態空間がすべてゼロかチェックする必要がある。言い換えると、
          * 必要なマスクをして余分な部分を排除してからゼロチェックする
          * べきである。
          *
          * @return true なら状態空間がすべてゼロ
+         *\endjapanese
+         *
+         *\english
+         * Check if bits in internal state are all zero.
+         *
+         * When using an incomplete array like Mersenne Twister,
+         * user should check bits in true internal state.
+         *\endenglish
          */
         virtual bool isZero() const = 0;
     };
