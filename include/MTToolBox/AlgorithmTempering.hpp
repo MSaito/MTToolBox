dbd14632b58eb4fde9224f4b8390f5ffb5680eab
Mutsuo Saito <saito@math.sci.hiroshima-u.ac.jp>
2013-10-15 14:57:35 +0900

    add English Document for AlgorithmTempering

--- include/MTToolBox/AlgorithmTempering.hpp
+++ include/MTToolBox/AlgorithmTempering.hpp
@@ -3,7 +3,13 @@
 /**
  * @file AlgorithmTempering.hpp
  *
+ *\japanese
  * @brief テンパリングパラメータ探索アルゴリズムのための抽象クラス
+ *\endjapanese
+ *
+ *\english
+ * @brief Abstract class for searching tempering parameters.
+ *\endenglish
  *
  * @author Mutsuo Saito (Hiroshima University)
  * @author Makoto Matsumoto (Hiroshima University)
@@ -26,32 +32,65 @@ namespace MTToolBox {
     /**
      * @class AlgorithmTempering
      *
+     *\japanese
      * @brief 疑似乱数生成器の高次元均等分布性を改善するために、テンパ
      * リングパラメータを探索するアルゴリズム
      *
-     * \b 注意： テンパリングパラメータの探索をしても十分良い高次元均等
+     * \warning テンパリングパラメータの探索をしても十分良い高次元均等
      * 分布が得られない場合は、状態遷移関数の変更を考慮した方がよい。状
      * 態遷移関数で十分ビットミックスされていない場合、単純なテンパリン
      * グで均等分布次元を最大化することはできないだろう。
      *
      * @tparam U 疑似乱数生成器の出力の型, 例えば uint32_t など。
+     *\endjapanese
+     *
+     *\english
+     * @brief Algorithm that search tempering parameters to improve
+     * dimension of equi-distribution of output of pseudo random number
+     * generator.
+     * \warning If you could not get high dimension of
+     * equi-distribution after tempering using parameters got by this
+     * algorithm, you should consider changing the design of random
+     * number generation algorithm.
+     *
+     * @tparam U type of output of pseudo random number generator,
+     * for example, uint32_t. Only unsigned numbers are allowed.
+     *\endenglish
      */
     template<typename U>
     class AlgorithmTempering {
     public:
 
         /**
+         *\japanese
          * テンパリングパラメータを探索する
          * @param[in, out] rand 疑似乱数生成器
          * @param[in] verbose 余分な情報を表示する
          * @returns 0
+         *\endjapanese
+         *
+         *\english
+         * Search tempering parameters. Searched parameters are
+         * set to \b rand.
+         * This process may consume large CPU time.
+         * @param rand pseudo random number generator
+         * @param verbose if true output redundant messages.
+         * @return always zero.
+         *\endenglish
          */
         virtual int operator()(TemperingCalculatable<U>& rand,
                                bool verbose = false) = 0;
 
         /**
+         *\japanese
          * LSB からのテンパリングをするのか
          * @return true LSBからのテンパリング
+         *\endjapanese
+         *
+         *\english
+         * Shows if searching tempering parameters is from LSBs.
+         * @return true if searching is from LSBs.
+         *\endenglish
          */
         virtual bool isLSBTempering() const {
             return false;
