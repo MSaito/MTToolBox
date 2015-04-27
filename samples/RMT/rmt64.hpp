73286cd6553dfa998698a7663e346350593e6dca
saito <sutagiro@qanzoo.sakura.ne.jp>
2015-04-27 14:11:34 +0900

    add rmt64

---
+++ samples/RMT/rmt64.hpp
@@ -0,0 +1,266 @@
+#pragma once
+#ifndef RMT64_HPP
+#define RMT64_HPP
+
+#include <MTToolBox/ReducibleTemperingCalculatable.hpp>
+#include <MTToolBox/ReducibleGenerator.hpp>
+#include <MTToolBox/MersenneTwister.hpp>
+#include <MTToolBox/util.hpp>
+#include <sstream>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+using namespace MTToolBox;
+using namespace NTL;
+
+class RMT64Search
+    : virtual public ReducibleTemperingCalculatable<uint64_t> {
+public:
+    RMT64Search(int mersenne_exponent, uint64_t v) {
+        mexp = mersenne_exponent;
+	maskb = 0;
+	maskc = 0;
+	parity = 0;
+        size = mexp / 64 + 1;
+        state = new uint64_t[size];
+        reverse = false;
+        seed(v);
+    }
+
+    RMT64Search(int mersenne_exponent, int position,
+               uint64_t matrix_a, uint64_t v) {
+        mexp = mersenne_exponent;
+        size = mexp / 64 + 1;
+        mata = matrix_a;
+        pos = position;
+	maskb = 0;
+	maskc = 0;
+	parity = 0;
+        state = new uint64_t[size];
+        reverse = false;
+        seed(v);
+    }
+
+    RMT64Search(int mersenne_exponent, int position,
+               uint64_t matrix_a, int mb, int mc, uint64_t v) {
+        mexp = mersenne_exponent;
+        size = mexp / 64 + 1;
+        mata = matrix_a;
+        pos = position;
+	maskb = mb;
+	maskc = mc;
+	parity = 0;
+        state = new uint64_t[size];
+        reverse = false;
+        seed(v);
+    }
+
+    RMT64Search(const RMT64Search& src)
+	: ReducibleTemperingCalculatable<uint64_t>() {
+        mexp = src.mexp;
+        size = src.size;
+        state = new uint64_t[size];
+        index = src.index;
+        reverse = src.reverse;
+        pos = src.pos;
+        mata = src.mata;
+	maskb = src.maskb;
+	maskc = src.maskc;
+	parity = src.parity;
+        for (int i = 0; i < size; i++) {
+            state[i] = src.state[i];
+        }
+    }
+
+    ~RMT64Search() {
+        delete[] state;
+    }
+
+    uint64_t generate() {
+        const uint64_t matrix_a[2] = {0, mata};
+	const uint64_t maska = UINT64_C(0x5555555555555555);
+	uint64_t x;
+	uint64_t y;
+        index = (index + 1) % size;
+        x = state[index];
+	y = state[(index + pos) % size];
+	y = y ^ (y << 17);
+	x = y ^ (x >> 1) ^ matrix_a[(int)(x & UINT64_C(1))];
+	state[index] = x;
+	x ^= (x >> sh1) & maska;
+	x ^= (x << sh2) & maskb;
+	x ^= (x << sh3) & maskc;
+	x ^= (x >> sh4);
+	return x;
+    }
+
+    uint64_t generate(int outBitLen) {
+        uint64_t u;
+        if (reverse) {
+            u = reverse_bit(generate());
+        } else {
+            u = generate();
+        }
+        uint64_t mask = 0;
+        mask = (~mask) << (64 - outBitLen);
+        return u & mask;
+    }
+
+    uint64_t getParityValue() const {
+	return state[index % size];
+    }
+
+    void setParityValue(uint64_t parity_value) {
+	state[index % size] = parity_value;
+	parity = parity_value;
+    }
+
+    void setOneBit(int bitPos) {
+	setZero();
+	int idx = bitPos / 64;
+	int r = bitPos % 64;
+	if (idx >= size) {
+#if defined(DEBUG)
+	    cout << "ERROR bitpos too big" << endl;
+#endif
+	    return;
+	}
+	state[idx] = 1 << r;
+#if defined(DEBUG) && 0
+	debug_print();
+	fflush(stdout);
+#endif
+    }
+
+    bool isZero() const {
+        for (int i = 0; i < size; i++) {
+            if (state[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void setZero() {
+        for (int i = 0; i < size; i++) {
+            state[i] = 0;
+        }
+        index = size - 1;
+    }
+
+    void add(EquidistributionCalculatable<uint64_t>& other) {
+        RMT64Search* that = dynamic_cast<RMT64Search *>(&other);
+        if (that == 0) {
+            throw std::invalid_argument(
+                "the adder should have the same type as the addee.");
+        }
+        for (int i = 0; i < size; i++) {
+            state[(index + i) % size] ^=
+		that->state[(that->index + i) % size];
+        }
+    }
+
+    EquidistributionCalculatable<uint64_t> * clone() const {
+        return new RMT64Search(*this);
+    }
+
+    void seed(uint64_t v) {
+        state[0]= v;
+        for (int i = 1; i < size; i++) {
+            state[i] = i
+		+ UINT64_C(6364136223846793005)
+		* (state[i - 1] ^ (state[i - 1] >> 62));
+        }
+        index = size - 1;
+    }
+
+    int bitSize() const {
+        return size * 64;
+    }
+
+    int getMexp() const {
+	return mexp;
+    }
+
+    void setUpParam(AbstractGenerator<uint64_t>& generator) {
+        pos = generator.generate() % size;
+	mata = generator.generate();
+    }
+
+    const std::string getHeaderString() {
+        return "mexp, pos, mata, parity, maskb, maskc";
+    }
+
+    const std::string getParamString() {
+	maskb = maskb >> sh2;
+	maskb = maskb << sh2;
+	maskc = maskc >> sh3;
+	maskc = maskc << sh3;
+        stringstream ss;
+        ss << dec << mexp << ",";
+        ss << dec << pos << ",";
+        ss << hex << setw(16) << setfill('0') << mata << ",";
+	ss << hex << setw(16) << setfill('0') << parity << ",";
+	ss << hex << setw(16) << setfill('0') << maskb << ",";
+	ss << hex << setw(16) << setfill('0') << maskc;
+        return ss.str();
+    }
+
+    void setTemperingPattern(uint64_t mask, uint64_t pattern, int src_bit) {
+	if (src_bit == 0) {
+	    maskb &= ~mask;
+	    maskb |= pattern & mask;
+	} else {
+	    maskc &= ~mask;
+	    maskc |= pattern & mask;
+	}
+    }
+
+    void setUpTempering() {
+    }
+
+    void setReverseOutput() {
+        reverse = true;
+    }
+
+    void resetReverseOutput() {
+        reverse = false;
+    }
+
+    bool isReverseOutput() {
+        return reverse;
+    }
+#if defined(DEBUG)
+    void debug_print() {
+        cout << "mexp:" << dec << mexp << endl;
+        cout << "size:" << dec << size << endl;
+        cout << "reverse:" << reverse << endl;
+        cout << "pos:" << dec << pos << endl;
+        cout << "mata:" << hex << mata << endl;
+	cout << "parity:" << hex << parity << endl;
+	cout << "maskb:" << hex << maskb << endl;
+	cout << "maskc:" << hex << maskc << endl;
+        for (int i = 0; i < size; i++) {
+            cout << "state[" << dec << i << "]:" << hex << state[i] << endl;
+        }
+        cout << "index:" << dec << index << endl;
+    }
+#endif
+private:
+    enum {sh1 = 29, sh2 = 17, sh3 = 37, sh4 = 43};
+    int mexp;
+    int size;
+    bool reverse;
+    int pos;
+    uint64_t mata;
+    uint64_t parity;
+    uint64_t maskb;
+    uint64_t maskc;
+    uint64_t * state;
+    int index;
+};
+
+#endif // RMT_HPP
---
+++ samples/RMT/rmt64dc.cpp
@@ -0,0 +1,245 @@
+/**
+ * @file rmt64dc.cpp
+ *
+ * @brief The main function of parameter generator of 64-bit Tiny
+ * Mersenne Twister.
+ *
+ * The functions in this file are simple. They parse the command line
+ * options and call all_in_one function which does almost all things.
+ * Users can change this file so that it fits to their applications
+ * and OS.
+ *
+ * @author Mutsuo Saito (Hiroshima University)
+ * @author Makoto Matsumoto (The University of Tokyo)
+ *
+ * Copyright (C) 2011 Mutsuo Saito, Makoto Matsumoto,
+ * Hiroshima University and The University of Tokyo.
+ * All rights reserved.
+ *
+ * The 3-clause BSD License is applied to this software, see
+ * LICENSE.txt
+ */
+#include <iostream>
+#include <iomanip>
+#include <stdint.h>
+#include <inttypes.h>
+#include <time.h>
+#include <unistd.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <MTToolBox/AlgorithmReducibleRT.hpp>
+#include <MTToolBox/MersenneTwister64.hpp>
+//#include <MTToolBox/AlgorithmBestBits.hpp>
+#include <MTToolBox/AlgorithmPartialBitPattern.hpp>
+#include <getopt.h>
+#include <errno.h>
+#include "rmt64.hpp"
+
+using namespace std;
+using namespace MTToolBox;
+
+class rmt_options {
+public:
+    int mexp;
+    bool verbose;
+    uint64_t seed;
+    std::string filename;
+    long count;
+};
+
+bool parse_opt(rmt_options& opt, int argc, char **argv);
+
+int search(rmt_options& opt, int count);
+
+/**
+ * parse command line option, and search parameters
+ * @param argc number of arguments
+ * @param argv value of arguments
+ * @return 0 if this ends normally
+ */
+int main(int argc, char** argv) {
+    rmt_options opt;
+    bool parse = parse_opt(opt, argc, argv);
+    if (!parse) {
+        return -1;
+    }
+    //cout << "mexp = " << dec << opt.mexp;
+    //cout << " seed = " << dec << opt.seed << endl;
+    return search(opt, opt.count);
+}
+
+/**
+ * search parameters using all_in_one function in the file search_all.hpp
+ * @param opt command line options
+ * @param count number of parameters user requested
+ * @return 0 if this ends normally
+ */
+int search(rmt_options& opt, int count) {
+    MersenneTwister64 mt(opt.seed);
+    RMT64Search g(opt.mexp, 1);
+    if (opt.verbose) {
+        time_t t = time(NULL);
+        cout << "search start at " << ctime(&t);
+    }
+    //static const int shifts[] = {17, 37};
+    //AlgorithmBestBits<uint64_t> tmp(64, shifts, 2, 15);
+    typedef AlgorithmPartialBitPattern<uint64_t, 64, 2, 63, 6> st64;
+    st64 st;
+    AlgorithmReducibleRecursionAndTempering<uint64_t> all(mt);
+    int i = 0;
+    cout << "# "
+	 << g.getHeaderString()
+	 << ", delta"
+	 << endl;
+    while (i < count) {
+        if (all.search(g, st, st, opt.verbose)) {
+            int delta = all.getDelta();
+            //g = all.get_rand();
+            //rmt64_param param = g.get_param();
+            //int weight = all.getWeight();
+            //GF2X poly = all.getCharacteristicPolynomial();
+            cout << g.getParamString() << "," << dec << delta << endl;
+            i++;
+        } else {
+            cout << "search failed" << endl;
+            break;
+        }
+    }
+    if (opt.verbose) {
+        time_t t = time(NULL);
+        cout << "search end at " << ctime(&t) << endl;
+    }
+    return 0;
+}
+
+static void output_help(string& pgm);
+
+/**
+ * command line option parser
+ * @param opt a structure to keep the result of parsing
+ * @param argc number of command line arguments
+ * @param argv command line arguments
+ * @param start default start value
+ * @return command line options have error, or not
+ */
+bool parse_opt(rmt_options& opt, int argc, char **argv) {
+    opt.verbose = false;
+    opt.mexp = 0;
+    opt.count = 1;
+    opt.seed = (uint64_t)clock();
+    opt.filename = "";
+    int c;
+    bool error = false;
+    string pgm = argv[0];
+    static struct option longopts[] = {
+        {"verbose", no_argument, NULL, 'v'},
+        {"file", required_argument, NULL, 'f'},
+        {"count", required_argument, NULL, 'c'},
+        {"seed", required_argument, NULL, 's'},
+        {NULL, 0, NULL, 0}};
+    errno = 0;
+    for (;;) {
+        c = getopt_long(argc, argv, "vs:f:c:", longopts, NULL);
+        if (error) {
+            break;
+        }
+        if (c == -1) {
+            break;
+        }
+        switch (c) {
+        case 's':
+            opt.seed = strtoull(optarg, NULL, 0);
+            if (errno) {
+                error = true;
+                cerr << "seed must be a number" << endl;
+            }
+            break;
+        case 'v':
+            opt.verbose = true;
+            break;
+        case 'f':
+            opt.filename = optarg;
+            break;
+        case 'c':
+            opt.count = strtoll(optarg, NULL, 10);
+            if (errno) {
+                error = true;
+                cerr << "count must be a number" << endl;
+            }
+            break;
+        case '?':
+        default:
+            error = true;
+            break;
+        }
+    }
+    argc -= optind;
+    argv += optind;
+    if (argc < 1) {
+        error = true;
+    } else {
+        long mexp = strtol(argv[0], NULL, 10);
+        static const int allowed_mexp[] = {521, 607, 1279, 2203,
+                                           2281, 3217, 4253, 4423,
+                                           9689, 9941, 11213, 19937,
+                                           21701, 23209, 44497, -1};
+        if (! errno) {
+            bool found = false;
+            for (int i = 0; allowed_mexp[i] > 0; i++) {
+                if (mexp == allowed_mexp[i]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (! found) {
+                error = true;
+            }
+        }
+        if (errno || error){
+            error = true;
+            cerr << "mexp must be one of ";
+            for (int i = 0; allowed_mexp[i] > 0; i++) {
+                cerr << dec << allowed_mexp[i] << " ";
+            }
+            cerr << endl;
+        }
+        opt.mexp = mexp;
+    }
+    if (!opt.filename.empty()) {
+        ofstream ofs(opt.filename.c_str());
+        if (ofs) {
+            ofs.close();
+        } else {
+            error = true;
+            cerr << "can't open file:" << opt.filename << endl;
+        }
+    }
+    if (error) {
+        output_help(pgm);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * showing help message
+ * @param pgm program name
+ */
+static void output_help(string& pgm) {
+    cerr << "usage:" << endl;
+    cerr << pgm
+         << " [-s seed] [-v] [-c count]"
+         << " [-f outputfile]"
+	 << " mexp"
+         << endl;
+    static string help_string1 = "\n"
+"--verbose, -v        Verbose mode. Output parameters, calculation time, etc.\n"
+"--file, -f filename  Parameters are outputted to this file. without this\n"
+"                     option, parameters are outputted to standard output.\n"
+"--count, -c count    Output count. The number of parameters to be outputted.\n"
+"--seed, -s seed      seed of randomness.\n"
+"mexp                 mersenne exponent.\n"
+	;
+    cerr << help_string1 << endl;
+}
